ac9e85457ec82dd9713efbe12ea064a76b773509
Julian Tuazon <jthrow00@gmail.com>
2020-02-05 17:09:44 -0800

    Created App class to access grades from student grade table api

---
+++ components/app.js
@@ -0,0 +1,22 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  getGrades() {
+    $.ajax({
+      url: "/api/grades",
+      success: this.handleGetGradesSuccess;
+      error: this.handleGetGradesError;
+    });
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  start() {
+    this.getGrades();
+  }
+}
