5159135025d2c089aa208c12939d12fa06d71062
Julian Tuazon <jthrow00@gmail.com>
2020-02-06 12:32:51 -0800

    Instantiated GradeForm class; added event listener upon submit on the html form element; modified App class constructor to accept GradeForm object

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+const formElement = document.querySelector("form");
+const gradeForm = new GradeForm(formElement);
 const header = document.querySelector("header");
 const pageHeader = new PageHeader(header);
 const table = document.getElementsByClassName("table")[0];
 const gradeTable = new GradeTable(table);
-const app = new App(gradeTable, pageHeader);
+const app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
